212d6f2b8eff816c22d185008359dc3fcfa2f3c9
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-06-27 10:12:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
+
+//Get no. of elements with class="drum"
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+//detecting button presses
+//for loop for adding event listener to all of the buttons (drums.Be more specific, which is better)
 for (var i = 0; i < numberOfDrumButtons; i++) {
-
+      //listening for clicks using anonymous function
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
     var buttonInnerHTML = this.innerHTML;
-
+     //calling makesound function and buttonAnimation function
     makeSound(buttonInnerHTML);
-
     buttonAnimation(buttonInnerHTML);
 
   });
-
 }
 
+//listening for keyboard presses
 document.addEventListener("keypress", function(event) {
-
   makeSound(event.key);
-
   buttonAnimation(event.key);
 
 });
 
-
-function makeSound(key) {
+//this function takes a character and checks through switch statement
+function  makeSound(key) {
 
   switch (key) {
     case "w":
@@ -60,46 +60,39 @@ function makeSound(key) {
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
-
-        
-
+      //default is like else statement
     default: console.log(key);
 
   }
 }
 
-
+//button animation
 function buttonAnimation(currentKey) {
 
-  var activeButton = document.querySelector("." + currentKey);
-
+  var activeButton = document.querySelector("." + currentKey );
+      //applying class .pressed from css
   activeButton.classList.add("pressed");
-
+      //using this to remove above applied css (0.1 seconds)
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
 
 }
 
+
+//pun in footer
 function show_hide()
 {
    var myAnswer = document.getElementById('answer');
-
-   var displaySetting = myAnswer.style.display;
-
-   var quizButton = document.getElementsByClassName('quiz-button');
-
+   var displaySetting = myAnswer.style.display; 
    if(displaySetting=="inline-block"){
        myAnswer.style.display = 'none';
-       
-       quizButton.innerHTML = 'Show Answer';
-       
    }
    else
    {
        myAnswer.style.display = 'inline-block';
-       quizButton.innerHTML = 'Hide Answer';
        var joke = new Audio("sounds/Joke Sound.mp3");
        joke.play();
    }
 }
+
